440e5b815bc87a2a92a85181d53cc298b17f4a7e
Raymond Gasper <raymond.j.gasper@gmail.com>
2021-02-24 21:13:25 -0500

    got a pipeline goin

---
+++ stages/concat.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import glob
+import os
+
+# NOTE must be run from repo root.
+
+files = glob.glob("data/initial_features/*")
+dfs = []
+for file in files:
+    dfs.append(pd.read_csv(file))
+all_df = pd.concat(dfs)
+all_df.to_csv("data/concatenated_battery_profile.csv")
