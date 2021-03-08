c70787616c18d4c5021ddb4383c2158574d5307e
Raymond Gasper <raymond.j.gasper@gmail.com>
2021-03-08 09:32:59 -0500

    remove deprecated experiment writeup

--- stages/identify_synthetic_y_model.py
+++ stages/identify_synthetic_y_model.py
@@ -1,3 +1,5 @@
+# DEPRECATED - This was just a little experiment
+
 import pandas as pd
 import numpy as np
 from scipy import optimize
@@ -5,17 +7,20 @@ import matplotlib.pyplot as plt
 import pickle
 
 df_y = pd.read_csv("data/target.csv")
-df_y['t_days'] = (df_y.timestamp-df_y.timestamp[0])/(24*3600)
-df_y['y'] = np.abs(df_y.Q_discharge)
+df_y["t_days"] = (df_y.timestamp - df_y.timestamp[0]) / (24 * 3600)
+df_y["y"] = np.abs(df_y.Q_discharge)
 
 # Define function for calculating a power law, and an error function
-capacityFadeModel = lambda q0, t, amp, power: q0 - (amp*t)**power
+capacityFadeModel = lambda q0, t, amp, power: q0 - (amp * t) ** power
+
+
 def error(p, x, y):
     y0 = y[0]
     yPred = capacityFadeModel(y0, x, p[0], p[1])
-    return (y-yPred)
+    return y - yPred
 
-params, _ = optimize.leastsq(error, [1,1], args=(df_y.t_days.values, df_y.y.values))
 
-with open('models/params.txt', 'w') as file:
-    file.write(','.join(str(p) for p in params))
+params, _ = optimize.leastsq(error, [1, 1], args=(df_y.t_days.values, df_y.y.values))
+
+with open("models/params.txt", "w") as file:
+    file.write(",".join(str(p) for p in params))
