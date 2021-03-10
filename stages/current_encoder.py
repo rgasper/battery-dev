8fdc6b599f8ebd09d410187c625d90173cad5ff7
Raymond Gasper <raymond.j.gasper@gmail.com>
2021-03-09 22:10:07 -0500

    checked smoothed cycles; added some handmade diagrams

---
+++ stages/current_encoder.py
@@ -0,0 +1,7 @@
+import numpy as np
+import torch as T
+
+device = T.device('cpu')
+
+class AutoEncoder:
+    # 64-32-16-2
--- stages/cycle_labeler.py
+++ stages/cycle_labeler.py
@@ -15,11 +15,14 @@ current_peaks, properties = find_peaks(smooth_data.current, prominence=prominenc
 cycle_edges = current_peaks.tolist()
 cycle_edges.append(len(raw_data.index) - 1)  # the last cycle just goes off to the end of the plot
 raw_data["cycle"] = None
+smooth_data["cycle"] = None
 previous_peak_ind = 0
 for cycle_number, peak_ind in enumerate(cycle_edges):
     start_timestamp = raw_data.index[previous_peak_ind]
     stop_timestamp = raw_data.index[peak_ind]
     previous_peak_ind = peak_ind
     raw_data.loc[start_timestamp:stop_timestamp, "cycle"] = cycle_number
+    smooth_data.loc[start_timestamp:stop_timestamp, "cycle"] = cycle_number
 
 raw_data.to_csv("data/raw_cycle_labeled.csv", index=True)
+smooth_data.to_csv("data/smooth_cycle_labeled.csv", index=True)
